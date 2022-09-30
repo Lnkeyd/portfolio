f5edcd6fee10ccc397eda62843d711e8f4c904f5
Lnkeyd <the.lnkeyd@gmail.com>
2022-10-01 02:00:57 +0300

    create template for my portfolio website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbSeparator,
+  } from '@chakra-ui/react'
+
+
+const Navbar = () => {
+
+    // wrapper + ifMobile ? hamburgerMenu : laptopMenu
+
+  return (
+    <div>
+        <Breadcrumb fontWeight='medium' fontSize='3xl'
+                    separator=''>
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>About Me</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+                <BreadcrumbLink href='#'>Skills</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href='#'>Projects</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+            </BreadcrumbItem>
+        </Breadcrumb>
+    </div>
+  )
+}
+
+export default Navbar
